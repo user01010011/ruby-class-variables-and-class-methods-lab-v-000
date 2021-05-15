3f05564bd2904fe22fd1b7b136dcc472de94f1ef
Shasha Yan <shashayan@gmail.com>
2021-05-15 22:00:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ attr_accessor :name, :artist, :genre
   @@count = 0
   @@genres = [ ]
   @@artists = [ ]
-  
+
   def initialize(name, artist, genre)
     @@count+= 1
     @@genres = [ ] << genre unless @@genres.include?(genre)
